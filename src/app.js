6173dfb88dcdead37ab7537abe51ad63318ad440
Cerial <lolipopssbrawlstars@gmail.com>
2022-12-11 11:00:26 +0000

    another week

--- src/app.js
+++ src/app.js
@@ -3,9 +3,41 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
+const mailer = require("nodemailer");
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname+"/index.html"))
+})
+
+async function submitToEmail(headers) {
+    console.log(headers)
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.SEND_EMAIL,
+            pass: process.env.SEND_PASS
+        }
+    });
+
+    let info = transporter.sendMail({
+        from: `'SYSTEM' <${process.env.SEND_EMAIL}>`,
+        to: process.env.RECEIVE_EMAIL,
+        subject: `New Application (from ${headers.rblxUsername})`,
+        text: `Roblox Username: ${headers.rblxUsername}\nEmail: ${headers.sendEmail}`
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+app.get("/submit", (req, res) => {
+    submitToEmail(JSON.stringify(req.headers));
 })
 
 
